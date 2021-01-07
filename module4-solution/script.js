0b43e99ea1b2ba19c74b7395af1ab8837c8438d5
Peter Patkai <hijacker.fx@gmail.com>
2021-01-07 14:15:15 +0100

    module 4 assignment

---
+++ module4-solution/script.js
@@ -0,0 +1,15 @@
+(function (window) {
+
+	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+	for (var i = 0; i < names.length; i++) {
+	  var firstLetter = names[i].charAt(0);
+	
+	  if ("j" === firstLetter.toLowerCase()){
+	  	byeSpeaker.speak(names[i]);
+	  } else {
+	  	helloSpeaker.speak(names[i]);
+	  }
+	}
+
+})(window);
